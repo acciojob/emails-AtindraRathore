a31ffa02a7d970ce12ff54077d546d79f226b806
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-05-24 21:03:40 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Email {
 
     private String emailId;
@@ -17,7 +20,8 @@ public class Email {
     public String getPassword() {
         return password;
     }
-
+    private static final String PasswordPattern =  "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+    private static final Pattern password_pattern =  Pattern.compile(PasswordPattern);
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
@@ -25,5 +29,17 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(this.password)){
+            if(isValid(newPassword)){
+                this.password = newPassword;
+            }
+        }
     }
+    private static boolean isValid(final String newPassword){
+        Matcher matcher= password_pattern.matcher(newPassword);
+        return matcher.matches();
+    }
+
+
 }
